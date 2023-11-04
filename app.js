ae52bffc0abeb6b6b40584c4d66f61cfa782a7a5
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-11-04 15:21:06 +0530

    using async await

--- app.js
+++ app.js
@@ -11,136 +11,121 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  console.log("req", req);
+app.use(async (req, res, next) => {
+  try {
+    console.log("req", req);
 
-  const token = req.headers.authorization;
-  console.log(token);
-  if (token) {
-    const decodedToken = jwt.verify(token, "your-secret-key",);
-    const userId = decodedToken.userId;
-    console.log("userId", userId);
+    const token = req.headers.authorization;
+    console.log(token);
+    if (token) {
+      const decodedToken = jwt.verify(token, "your-secret-key");
+      const userId = decodedToken.userId;
+      console.log("userId", userId);
 
-    user
-      .findByPk(userId)
-      .then((user) => {
+      const user = await user.findByPk(userId);
+      if (user) {
         req.user = user;
         next();
-      })
-      .catch((err) => {
-        console.log(err);
-        next();
-      });
-  } else {
-    next();
+      } else {
+        throw new Error("User not found");
+      }
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 });
 
 //signup...........
 
-app.post("/signup", (req, res) => {
-  console.log("sign", req.body);
-  const { name, email, password } = req.body;
+app.post("/signup", async (req, res) => {
+  try {
+    console.log("sign", req.body);
+    const { name, email, password } = req.body;
 
-  //check email in our database
+    //check email in our database
 
-  user
-    .findOne({ where: { email } })
-    .then((existingUser) => {
-      if (existingUser) {
-        return res.status(400).json({ error: "email already exists" });
+    const existingUser = await user.findOne({ where: { email } });
+    if (existingUser) {
+      return res.status(400).json({ error: "email already exists" });
+    }
+
+    //hash the password
+    const hasPassword = await bcrypt.hash(password, 10);
+
+    //create new user with the hashed password
+    const newUser = await user.create({ name, email, password: hasPassword });
+
+    //generate a jwt token for the new user
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        name: user.name,
+      },
+      "your-secret-key",
+      {
+        expiresIn: "1h",
       }
+    );
 
-      //hash the password
-
-      bcrypt.hash(password, 10).then((hasPassword) => {
-        //create new user with the hashed password
-        user
-          .create({ name, email, password: hasPassword })
-          .then((newuser) => {
-            //generate a jwt token for the new user
-            const token = jwt.sign(
-                {
-                  userId: user.id,
-                  name: user.name,
-                },
-                "your-secret-key",
-                {
-                  expiresIn: "1h",
-                }
-              );
-              
-            res.json({ token, userId: newuser.id });
-          })
-          .catch((err) => {
-            console.error(error);
-            res.status(500).json({ error: "Internal server error" });
-          });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    res.json({ token, userId: newuser.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 //login................
 
-app.post("/login",(req, res) => {
-  const { email, password } = req.body;
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-  user
-    .findOne({ where: { email } })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: "Invalid email" });
+    const user = await user.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid email" });
+    }
+
+    //compare the provided password with hashpassword
+
+    const result = await bcrypt.compare(password, user.password);
+    if (!result) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+
+    //generate a jwt token
+
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        name: user.name,
+      },
+      "your-secret-key",
+      {
+        expiresIn: "1h",
       }
+    );
 
-      //compare the provided password with hashpassword
+    //return token & userId
 
-      bcrypt
-        .compare(password, user.password)
-        .then((result) => {
-          if (!result) {
-            return res.status(401).json({ error: "Invalid password" });
-          }
-
-          //generate a jwt token
-
-          const token = jwt.sign(
-            {
-              userId: user.id,
-              name: user.name,
-            },
-            "your-secret-key",
-            {
-              expiresIn: "1h",
-            }
-          );
-
-          //return token & userId
-
-          res.json({ token, userId: user.id });
-        })
-        .catch((err) => {
-          console.log(err);
-
-          res.status(500).json({ erroe: "internal server error" });
-        });
-    })
-    .catch((err) => {
-      console.log(err);
-
-      res.status(500).json({ erroe: "internal server error" });
-    });
+    res.json({ token, userId: user.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-sequelize.sync()
-.then((result)=>{
-    return user.findByPk(1)
-}).then((user)=>{
-    console.log(user)
-    app.listen(3000)
-})
-.catch((err)=>{
-    console.log(err)
-})
+sequelize
+  .sync()
+  .then((result) => {
+    return user.findByPk(1);
+  })
+  .then((user) => {
+    console.log(user);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
