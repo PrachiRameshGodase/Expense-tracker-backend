5c509e62578c7b0ca3d35e868f69bd5aa35b08cd
Prachi Ramesh Godase <prachigodase2@gmail.com>
2023-11-13 09:37:24 +0530

    Leaderboard Optimisation

--- app.js
+++ app.js
@@ -227,25 +227,39 @@ app.put("/razorpay/transaction/:orderId", async (req, res) => {
 app.get("/premium/showleaderboard",async(req,res)=>{
   try{
     //fetch all users from the database
-    const users=await user.findAll()
+    const leaderboard=await user.findAll({
+      attributes:[
+        "id",
+        "name",
+        [sequelize.fn('sum',sequelize.col('expenses.amount')),"totalExpense"]
+      ],
+      include:[
+        {
+          model:expenses,
+          attributes:[]
+        }
+      ],
+      group:["id"],
+      order:[["totalExpense","DESC"]]
+    })
 
     //Initialize an array to store user expenses
 
-    const userExpenses=[]
+    // const userExpenses=[]
 
 
-    for(const user of users){
+    // for(const user of users){
 
-      const expense=await expenses.findAll({where:{userId:user.id}})
+    //   const expense=await expenses.findAll({where:{userId:user.id}})
 
-      const totalExpense=expense.reduce((sum,expense)=>
-        sum+expense.amount
-      ,0)
+    //   const totalExpense=expense.reduce((sum,expense)=>
+    //     sum+expense.amount
+    //   ,0)
 
-      userExpenses.push({name:user.name,totalExpense})
-    }
+    //   userExpenses.push({name:user.name,totalExpense})
+    // }
 
-    const leaderboard = userExpenses.sort((a, b) => b.totalExpense - a.totalExpense);
+    // const leaderboard = userExpenses.sort((a, b) => b.totalExpense - a.totalExpense);
 
 res.json({ leaderboard });
 
